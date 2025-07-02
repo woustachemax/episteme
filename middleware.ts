7fa9be2bb60dacc1c4216f596f740b008ce2f1c6
Siddharth Thakkar <sid2011thakkar@gmail.com>
2025-07-02 17:27:08 -0400

    auth-more

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { getToken } from 'next-auth/jwt'
+import { NextResponse, NextRequest } from 'next/server'
+ 
+export async function middleware(request: NextRequest) {
+
+    const token = await getToken({req: request})
+    const url =  request.nextUrl
+    if(token && (
+        url.pathname.startsWith('/signin') ||
+        url.pathname.startsWith('/signup')
+
+    )){
+        return NextResponse.redirect(new URL('/home', request.url) )
+    }
+}
+ 
+export const config = {
+  matcher: [
+    '/signin',
+    '/signup'
+
+  ],
+}
